837653108c99c7f9b0980c28a43227831cc2adba
tobi <noggnzzz@gmail.com>
2022-12-22 04:14:59 -0600

    Pasa a realización de Menú y promociones

--- scripts.js
+++ scripts.js
@@ -1,4 +1,19 @@
 const element = document.getElementById("preloader");
+const header = document.getElementsByTagName("header")[0];
+const main = document.getElementsByTagName("main")[0];
+const footer = document.getElementsByTagName("footer")[0];
+
+setTimeout(function(){
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+  header.style.display = 'block';
+  main.style.display = 'block';
+  footer.style.display = 'block';
+}, 4500);
+
 setTimeout(function(){
   element.remove();
 }, 5000);
